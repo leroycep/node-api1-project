d7ee722116857dbd238b23f1ff31e7436a9ce366
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-06 14:14:01 -0600

    Add GET `/api/users` path

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const express = require("express");
 
-console.log("\nHello, world!\n");
+const server = express();
+const port = 8482;
 
+// Server state
+let users = [];
+let nextId = 0;
+
+const addUser = (newUser) => {
+  let id = nextId;
+  nextId += 1;
+  users.push({ ...newUser, id });
+};
+
+addUser({ name: "John Doe", bio: "Goes by another name usually" });
+
+// Server Routes
+server.get("/api/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+// Start listening!
+server.listen(port, () => {
+  console.log(` == Server listening on port ${port} ==`);
+});
